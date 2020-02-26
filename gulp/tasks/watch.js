8f6030fa9b3ddeb6ab7f56c9c123aa7ab35a1d4f
Vladymyr Davii <vladimir.davii@gmail.com>
2020-02-26 23:57:39 +0200

    little fix

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,10 +1,10 @@
 module.exports = function () {
     $.gulp.task('watch', function () {
-        $.gulp.watch('./dev/pug/**/*.pug', $.gulp.series('pug'));
-        $.gulp.watch('./dev/static/styles/**/*.sass', $.gulp.series('styles:dev'));
+        $.gulp.watch('./dev/pug/**/*.pug', {usePolling: true}, $.gulp.series('pug'));
+        $.gulp.watch('./dev/static/styles/**/*.sass', {usePolling: true}, $.gulp.series('styles:dev'));
         $.gulp.watch(['./dev/static/images/general/**/*.{png,jpg,gif,svg}',
-            './dev/static/images/content/**/*.{png,jpg,gif,svg}'], $.gulp.series('img:dev'));
-        $.gulp.watch('./dev/static/images/svg/*.svg', $.gulp.series('svg'));
-        $.gulp.watch('./dev/static/js/**/*.js', $.gulp.series('js:dev'));
+            './dev/static/images/content/**/*.{png,jpg,gif,svg}'], {usePolling: true}, $.gulp.series('img:dev'));
+        $.gulp.watch('./dev/static/images/svg/*.svg', {usePolling: true}, $.gulp.series('svg'));
+        $.gulp.watch('./dev/static/js/**/*.js', {usePolling: true}, $.gulp.series('js:dev'));
     });
 };
