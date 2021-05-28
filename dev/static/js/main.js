64f8e601c2e2440652fbf308a144f834b2bccd30
Vladimir Davii <vladimir.davii@gmail.com>
2021-05-28 14:21:47 +0300

    fix husky stylelint

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -2,6 +2,7 @@ import $ from 'jquery'
 // window.jQuery = $; window.$ = $ // import module example (npm i -D jquery)
 // required if js from vendor folder
 
+
 $(document).ready(function() {
   console.log('hello world!');
 
